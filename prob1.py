be24a154c18184caff109ecb10b90bfd2ddcc621
Cacoph0ny <t1nk3rt0y@gmail.com>
2018-02-18 15:29:44 -0800

    Organization/Updates

--- prob1.py
+++ prob1.py
@@ -11,15 +11,15 @@ def main():
     packageWeight=float(input("Package weight in pounds:\t"))
     if packageWeight <= 2:
         packagePrice=packageWeight*1.5
-        print("It would be %.2f to ship that package" %(packagePrice))
+        print("It would be %.2f to ship the package" %(packagePrice))
     elif packageWeight > 2 and packageWeight <= 6:
         packagePrice=packageWeight*3
-        print("It would be %.2f to ship that package" %(packagePrice))
+        print("It would be %.2f to ship the package" %(packagePrice))
     elif packageWeight > 6 and packageWeight <=10:
         packagePrice=packageWeight*4
-        print("It would be %.2f to ship that package" %(packagePrice))
+        print("It would be %.2f to ship the package" %(packagePrice))
     else:
         packagePrice=packageWeight*4.75
-        print("It would be %.2f to ship that package" %(packagePrice))
+        print("It would be %.2f to ship the package" %(packagePrice))
 
 main()
