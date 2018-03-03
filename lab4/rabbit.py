f1fea1a7d7ab9fb5b58f94d5ddd9046cc192790e
Cacoph0ny <t1nk3rt0y@gmail.com>
2018-03-02 20:50:27 -0800

    Finished Lab4

--- lab4/rabbit.py
+++ lab4/rabbit.py
@@ -10,14 +10,18 @@
 def main():
     wolfPop=float(input("Starting wolf population:\t"))
     rabbitPop=float(input("Starting rabbit population:\t"))
-    grassArea=float(input("Starting grass area (in square yards):\t"))
-    wolfGrow=float(input("Wolf growth rate annually (in percent):\t"))
-    rabbitGrow=float(input("Rabbit growth rate annually (in percent):\t"))
-    grassGrow=float(input("Grass growth rate annually (in percent):\t"))
+    grassArea=float(input("Starting grass area:\t\t"))
+    wolfGrow=float(input("Wolf growth rate annually:\t"))
+    rabbitGrow=float(input("Rabbit growth rate annually:\t"))
+    grassGrow=float(input("Grass growth rate annually:\t"))
     wolfPopSmall=wolfPop
     wolfPopBig=wolfPop
-    rabbitPopSmall=0
-    rabbitPopBig=0
+    rabbitPopSmall=rabbitPop
+    rabbitPopBig=rabbitPop
+    wolfSmallCount=0
+    wolfBigCount=0
+    rabbitSmallCount=0
+    rabbitBigCount=0
     counter=0
     wolfCount=0
     while counter <= 20:
@@ -26,15 +30,38 @@ def main():
             print("\n%d\t %.0f\t\t\t %.0f\t\t\t %.2f" %(counter,wolfPop,rabbitPop,grassArea))
             counter += 1
         else:
-            grassArea= (grassArea * (1 + grassGrow/100) - (rabbitPop * 1.2))//1
+            grassArea= (grassArea * (1 + grassGrow/100) - (rabbitPop * 1.2))
             rabbitPop= (rabbitPop * (1 + rabbitGrow/100) - (wolfPop*50))//1
+            if rabbitPop > rabbitPopBig:
+                rabbitPopBig = rabbitPop
+                rabbitBigCount = counter
+            if rabbitPop < rabbitPopSmall:
+                rabbitPopSmall = rabbitPop
+                rabbitSmallCount = counter
             if wolfCount == 5:
-                wolfPop /= 2
+                wolfPop = (wolfPop/2)//1
                 wolfCount = 1
+                if wolfPop > wolfPopBig:
+                    wolfPopBig = wolfPop
+                    wolfBigCount = counter
+                if wolfPop < wolfPopSmall:
+                    wolfPopSmall = wolfPop
+                    wolfSmallCount = counter
             else:
                 wolfPop = (wolfPop * (1+(wolfGrow/100)))//1
                 wolfCount += 1
+                if wolfPop > wolfPopBig:
+                    wolfPopBig = wolfPop
+                    wolfBigCount = counter + 1
+                if wolfPop < wolfPopSmall:
+                    wolfPopSmall = wolfPop
+                    wolfSmallCount = counter + 1
             print("%d\t %.0f\t\t\t %.0f\t\t\t %.2f" %(counter,wolfPop,rabbitPop,grassArea))
             counter += 1
+    print("Wolf population was minimum at %d in the year %d" %(wolfPopSmall, wolfSmallCount))
+    print("Wolf population was maximum at %d in the year %d" %(wolfPopBig, wolfBigCount))
+    print("Rabbit population was minimum at %d in the year %d" %(rabbitPopSmall, rabbitSmallCount))
+    print("Rabbit population was maximum at %d in the year %d" %(rabbitPopBig, rabbitBigCount))
+
 
 main()
